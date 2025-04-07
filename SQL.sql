d90362e6d6b61c23728bb505d51a45ac97b60bbb
ArtemiPodporin <120027758+ArtemiPodporin@users.noreply.github.com>
2025-04-07 11:36:37 +0300

    Update SQL.sql

--- SQL.sql
+++ SQL.sql
@@ -23,3 +23,21 @@ SELECT * FROM inimene;
 
 --table kustutamine
 DROP table inimene;
+
+
+
+--andnete lisamine
+--DDL - date definition language
+--DML - date maniplation language
+
+INSERT INTO inimene (nimi, synniag, telefon, pikkus, opilaskodu)
+VALUES
+('Mati Kaskkk', '2041-12-30', '1261536', 94.5, 0),
+('Mati Kaskkkkk', '2031-12-30', '1263546', 92.5, 0),
+('Mati Kaskkkkkkk', '2051-12-30', '1361546', 91.5, 0),
+('Mati Kaskkkkkkkkk', '2061-12-30', '1261346', 96.5, 0)
+
+select * from inimene;
+delete from inimene
+
+where inimeneID=3;
